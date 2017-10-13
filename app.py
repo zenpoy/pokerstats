61e47b9ebab03677827991d9b6f60ce6fb3b7477
jabramso <jonathan,abramson@intel.com>
2017-10-13 15:40:58 +0300

    added mongodb init supoport and RESTful framework for players

--- app.py
+++ app.py
@@ -1,13 +1,74 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
+from mongoengine import *
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+mongodb_uri = os.environ.get('MONGODB_URI', 'localhost:27017')
+
+connect("pokerstats", host=mongodb_uri)
+
+class Player(Document):
+	name = StringField(required=True, unique=True, max_length=200)
+
+
+class GamePlayerStats(Document):
+	player = ReferenceField("Player", required=True)
+	game = ReferenceField("Game", required=True)
+	cash_in = FloatField()
+	good_all_in = ListField(field=DateTimeField)
+	bad_all_in = ListField(field=DateTimeField)
+	cash_out = FloatField()
+
+
+class Game(Document):
+	name = StringField(max_length=200)
+	date = DateTimeField()
+	cash = FloatField()
+
+
+@app.route('/', methods=['GET'])
+@app.route('/players', methods=['GET'])
+def get_players():
+	return Player.objects.to_json()
+
+@app.route('/players/<player_id>', methods=['GET'])
+def get_player(player_id):
+	p = Player.objects(id=player_id)
+	return p.to_json(), 200
+
+@app.route('/players', methods=['POST'])
+def create_player():
+	# TODO: add check for is json
+	json_data = request.get_json()
+	p = Player(**json_data)
+	try:
+		p.save()
+	except NotUniqueError as e:
+		return jsonify({'error' : e.message}), 200
+	return p.to_json(), 201
+
+@app.route('/players/<player_id>', methods=['DELETE'])
+def delete_player(player_id):
+	Player.objects(id=player_id).delete()
+	return jsonify({}), 200
+
+@app.route('/players/<player_id>', methods=['PUT'])
+def update_player(player_id):
+	# TODO: add check for is json
+	json_data = request.get_json()
+	p = Player.objects(id=player_id)
+	p.update(**json_data)
+	return p.to_json(), 200
+
+
+
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+    # connect to the mongodb database
